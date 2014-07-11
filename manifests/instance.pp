727878ad7e8d703c01f30e1ba8ca3a933b00f6b5
Dario Sestero <dsestero@gmail.com>
2014-07-11 17:50:06 +0200

    Temporarily commented out the substitution of the tarball includes/DefaultSettings.php with the template for three reasons: 1) when switching from a MediaWiki version to a subsequent one the DefaultSettings.php file may change significantly, as it was the case between MediaWiki-1.22.8 and MediaWiki-1.23.1; using the old template was not possible and caused the wiki aspect to not function properly (missed menus and bars, etc). I specify the tarball externally in a hiera file but then i had to change the template. 2) As strongly recommended at the beginning of the DefaultSettings.php file, it should not be edited. It is intended to contain all the default settings, specific for that version of the MediaWiki. All wiki specific settings should be done in the LocalSettings.php file. 3) More importantly, it caused problems when one has more than one wiki instance if he specifies slightly different settings in different instances (I had different values for the smtp_idhost parameter because I wanted to use the wiki instance name as part of the sender address). One could move the SMTP settings from the mediawiki::instance define to the mediawiki class, hence ensuring the SMTP parameters are not messed up between different wiki instances (but remains the problem of the template when upgrading the MediaWiki software) or leave them in the mediawiki::instance define and writing them in the LocalSettings.php file by using a mechanism different from the template, for example by using the puppetlabs-inifile module or the file_line resource from stdlib.

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -137,11 +137,11 @@ define mediawiki::instance (
         ensure   => directory,
       }
 
-      # MediaWiki DefaultSettings
-      file { "${mediawiki_conf_dir}/${name}/includes/DefaultSettings.php":
-        ensure  =>  present,
-        content =>  template('mediawiki/DefaultSettings.php.erb'),  
-      }
+#      # MediaWiki DefaultSettings
+#      file { "${mediawiki_conf_dir}/${name}/includes/DefaultSettings.php":
+#        ensure  =>  present,
+#        content =>  template('mediawiki/DefaultSettings.php.erb'),  
+#      }
 
       # Each instance needs a separate folder to upload images
       file { "${mediawiki_conf_dir}/${name}/images":
